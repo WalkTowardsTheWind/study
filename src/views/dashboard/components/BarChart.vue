8cf00eb1b4d368f2ce783c187ea5d3be299954fc
leiyuhang <2465589779@qq.com>
2023-08-14 16:34:18 +0800

    fix: :bug: 控制台盈利看板天机

--- src/views/dashboard/components/BarChart.vue
+++ src/views/dashboard/components/BarChart.vue
@@ -10,34 +10,30 @@
       class="dashboard-bar-content relative"
       :style="{ height: `${boxHeight - 40}px` }"
     >
-      <!--      <div class="dashboard-bar-content-search">-->
-      <!--        <div class="dashboard-bar-content-search-quick">-->
-      <!--          <span-->
-      <!--            v-for="item in typeMap"-->
-      <!--            :key="item.value"-->
-      <!--            :class="{ active: activeType === item.value }"-->
-      <!--            @click="handleChange(item)"-->
-      <!--          >-->
-      <!--            {{ item.label }}-->
-      <!--          </span>-->
-      <!--        </div>-->
-      <!--        &lt;!&ndash;        <div class="dashboard-bar-content-search-date">&ndash;&gt;-->
-      <!--        &lt;!&ndash;          <zxn-date-range class="w-272px mr-16px" />&ndash;&gt;-->
-      <!--        &lt;!&ndash;          <el-button type="primary">查询</el-button>&ndash;&gt;-->
-      <!--        &lt;!&ndash;        </div>&ndash;&gt;-->
-      <!--      </div>-->
-      <!--      <div-->
-      <!--        :id="id"-->
-      <!--        :class="className"-->
-      <!--        :style="{ height: `${boxHeight - 110}px`, width }"-->
-      <!--      />-->
-      <empty-box />
+      <div class="dashboard-bar-content-search">
+        <div class="dashboard-bar-content-search-quick">
+          <span
+            v-for="item in typeMap"
+            :key="item.value"
+            :class="{ active: activeType === item.value }"
+            @click="handleChange(item)"
+          >
+            {{ item.label }}
+          </span>
+        </div>
+      </div>
+      <div
+        :id="id"
+        :class="className"
+        :style="{ height: `${boxHeight - 40}px`, width }"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { dateUtil } from "@/utils/dateUtil";
+import { getFinanceList } from "@/api/money";
 import * as echarts from "echarts";
 
 const props = defineProps({
@@ -56,77 +52,83 @@ const props = defineProps({
   },
 });
 const typeMap = [
-  { label: "今年", value: 1 },
-  { label: "本季度", value: 2 },
+  { label: "上周", value: 2 },
+  { label: "本周", value: 1 },
+  { label: "上月", value: 4 },
   { label: "本月", value: 3 },
-  { label: "本周", value: 4 },
 ];
-const activeType = ref(1);
+const activeType = ref(3);
 const xAxisData: string[] = ref([]);
+const yAxisData = ref([]);
 const handleChange = (item: any) => {
   activeType.value = item.value;
 };
-const chartInits = () => {
-  setTimeout(() => {
-    const options = {
-      tooltip: {
-        trigger: "axis",
-        axisPointer: {
-          type: "shadow",
+const chartInits = async () => {
+  const params = { time_type: activeType.value };
+  const { data } = await getFinanceList(params);
+  xAxisData.value = [];
+  yAxisData.value = [];
+  data.income.forEach((it) => {
+    xAxisData.value.push(it.date_time);
+    yAxisData.value.push(it.amount);
+  });
+  const options = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    grid: {
+      left: "3%",
+      right: "0",
+      bottom: "3%",
+      top: "80px",
+      containLabel: true,
+    },
+    legend: {
+      data: ["收入"],
+      right: "300px",
+      top: "35px",
+      icon: "circle",
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: xAxisData.value,
+        axisTick: {
+          alignWithLabel: true,
         },
       },
-      grid: {
-        left: "3%",
-        right: "0",
-        bottom: "3%",
-        containLabel: true,
+    ],
+
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        name: "收入",
+        type: "line",
+        showSymbol: false,
+        smooth: true,
+        itemStyle: {
+          color: "#366ff4", // 折线颜色
+          symbol: "circle", // 圆点标记
+          symbolSize: 10, // 圆点大小
+          lineStyle: {
+            width: 1, // 折线宽度
+          },
+        },
+        data: yAxisData.value,
       },
-      xAxis: [
-        {
-          type: "category",
-          data: xAxisData.value,
-          axisTick: {
-            alignWithLabel: true,
-          },
-        },
-      ],
-      yAxis: [
-        {
-          type: "value",
-        },
-      ],
-      series: [
-        {
-          name: "Direct",
-          type: "line",
-          barWidth: "60%",
-          itemStyle: {
-            color: "#356FF3",
-          },
-          data: [],
-        },
-      ],
-    };
-    console.log(options, 2222);
-    chart.setOption(options);
-  }, 1000);
+    ],
+  };
+  console.log(options, 2222);
+  chart.setOption(options);
 };
 watch(
   activeType,
-  (val) => {
-    const currentMonth = new Date().getMonth() + 1;
-    switch (val) {
-      case 1:
-        console.log(new Date().getMonth());
-        xAxisData.value = [...new Array(currentMonth).keys()].map(
-          (it) => `${it + 1}月`
-        );
-        break;
-      case 2:
-        // console.log(dateUtil().quarter(2))
-        xAxisData.value = [];
-        break;
-    }
+  () => {
     chartInits();
   },
   {
@@ -140,7 +142,6 @@ let boxHeight = ref(0);
 
 onMounted(() => {
   const { bottom } = (dashboardBar as any).value.getBoundingClientRect();
-  console.log(bottom, "2222222222222");
   boxHeight.value = bottom < 360 ? 360 : bottom;
   nextTick(() => {
     // 图表初始化
@@ -155,11 +156,15 @@ onMounted(() => {
 <style lang="scss" scoped>
 .dashboard-bar {
   &-content {
+    position: relative;
     margin-top: 16px;
     background: #fff;
     border-radius: 4px;
 
     &-search {
+      position: absolute;
+      top: 0;
+      right: 0;
       display: flex;
       align-items: center;
       justify-content: flex-end;
