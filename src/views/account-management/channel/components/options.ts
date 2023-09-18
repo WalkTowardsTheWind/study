6911536c13f834d487be70e8a467953ef23fc560
wyr <wang_yuan_rui@123.com>
2023-09-18 16:07:59 +0800

    代码更新

--- src/views/account-management/channel/components/options.ts
+++ src/views/account-management/channel/components/options.ts
@@ -1,22 +1,22 @@
 export const settlement_type = [
-	{
-		label: "周结",
-		value: 1,
-	},
-	{ label: "日结", value: 2 },
+  {
+    label: "周结",
+    value: 1,
+  },
+  { label: "日结", value: 2 },
 ];
 
 export const collection_type = [
-	{
-		label: "公对私",
-		value: 1,
-	},
-	{
-		label: "公对公",
-		value: 2,
-	},
-	{
-		label: "私对私",
-		value: 3,
-	},
+  {
+    label: "公对私",
+    value: 1,
+  },
+  {
+    label: "公对公",
+    value: 2,
+  },
+  {
+    label: "私对私",
+    value: 3,
+  },
 ];
