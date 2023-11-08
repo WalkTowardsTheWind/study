88f95ba409ab79d332d765296a0e4fd817ca09a5
leiyuhang <2465589779@qq.com>
2023-11-08 17:29:50 +0800

    feat: :sparkles: icon添加

--- src/views/invoice/invoiceManager/components/addinvoice.vue
+++ src/views/invoice/invoiceManager/components/addinvoice.vue
@@ -26,7 +26,11 @@
         <el-input v-model="formItem.express_no" />
       </el-form-item>
       <el-form-item label="票样" prop="invoice_sample_url">
-        <multi-upload v-model="formItem.invoice_sample_url" :limit="20" />
+        <multi-upload
+          v-model="formItem.invoice_sample_url"
+          :limit="20"
+          multiple
+        />
       </el-form-item>
       <el-form-item label="物流面单" prop="express_url">
         <multi-upload v-model="formItem.express_url" :limit="1" />
