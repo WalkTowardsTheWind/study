0e1fb36196fe9d499523785ef3eb426f12ed3869
wyr <wang_yuan_rui@123.com>
2023-11-21 09:24:38 +0800

    feat: :sparkles: 合同列表 添加已拒签状态

--- src/views/contract-management/components/options.ts
+++ src/views/contract-management/components/options.ts
@@ -1,76 +1,78 @@
 export const contract_status = [
-	{
-		status: "",
-		label: "全部",
-	},
-	{
-		status: 0,
-		label: "待签署",
-	},
-	{
-		status: 1,
-		label: "签署中",
-	},
-	{
-		status: 2,
-		label: "已生效",
-	},
-	{
-		status: 3,
-		label: "已失效",
-	},
+  {
+    status: "",
+    label: "全部",
+  },
+  {
+    status: 0,
+    label: "待签署",
+  },
+  {
+    status: 1,
+    label: "签署中",
+  },
+  {
+    status: 2,
+    label: "已生效",
+  },
+  {
+    status: 3,
+    label: "已失效",
+  },
+  {
+    status: 4,
+    label: "已拒签",
+  },
 ];
 export const contract_types = [
-	{
-		value: 1,
-		label: "企业合同",
-	},
-	{
-		value: 2,
-		label: "渠道合同",
-	},
-	{
-		value: 3,
-		label: "其他合同",
-	},
-	{
-		value: 4,
-		label: "委托代征协议",
-	},
+  {
+    value: 1,
+    label: "企业合同",
+  },
+  {
+    value: 2,
+    label: "渠道合同",
+  },
+  {
+    value: 3,
+    label: "其他合同",
+  },
+  {
+    value: 4,
+    label: "委托代征协议",
+  },
 ];
 
 export const color = {
-	0: {
-		color: "#35C5F3",
-		background: "#DFF6FD",
-	},
-	1: {
-		color: "#1EE585",
-		background: "#DBFBEB",
-	},
-	2: {
-		color: "#356FF3",
-		background: "#DFE8FD",
-	},
-	3: {
-		color: "#333333",
-		background: "#DEDEDE",
-	},
-	4: {
-		color: "#f35036",
-		background: "#fde3df",
-	},
+  0: {
+    color: "#35C5F3",
+    background: "#DFF6FD",
+  },
+  1: {
+    color: "#1EE585",
+    background: "#DBFBEB",
+  },
+  2: {
+    color: "#356FF3",
+    background: "#DFE8FD",
+  },
+  3: {
+    color: "#333333",
+    background: "#DEDEDE",
+  },
+  4: {
+    color: "#f35036",
+    background: "#fde3df",
+  },
 };
 
-
 export const percolor = {
-	1: {
-		color: "#356FF3",
-		background: "#DFE8FD",
-	},
-	2: {
-		color: "#333333",
-		background: "#DEDEDE",
-	},
-
+  1: {
+    color: "#356FF3",
+    background: "#DFE8FD",
+  },
+  2: {
+    color: "#333333",
+    background: "#DEDEDE",
+  },
 };
