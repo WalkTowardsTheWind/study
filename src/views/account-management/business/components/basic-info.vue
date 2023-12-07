7c21be9e750b901244126038c90715fb11376fba
柳源 <ly1271512676@163.com>
2023-12-07 17:05:02 +0800

    fix: :bug: 禁用企业登录名称

--- src/views/account-management/business/components/basic-info.vue
+++ src/views/account-management/business/components/basic-info.vue
@@ -31,7 +31,11 @@
           </el-col>
           <el-col :span="8">
             <el-form-item label="企业登录名称">
-              <el-input v-model="formItem.account" :readonly="!isEdit" />
+              <el-input
+                v-model="formItem.account"
+                :readonly="!isEdit"
+                disabled
+              />
             </el-form-item>
             <el-form-item label="法定代表人">
               <el-input v-model="formItem.legal_person" :readonly="!isEdit" />
