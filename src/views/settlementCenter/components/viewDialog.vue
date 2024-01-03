38af604ba3bbdd4941aee6e27c71ecfdd264e836
柳源 <ly1271512676@163.com>
2024-01-03 11:25:01 +0800

    feat: :sparkles: 修改发佣确认单

--- src/views/settlementCenter/components/viewDialog.vue
+++ src/views/settlementCenter/components/viewDialog.vue
@@ -1,80 +1,70 @@
 <template>
   <el-dialog
-    custom-class="my-dialog"
     v-model="dialogVisible"
+    title="发佣确认"
+    modal-class="enterprise-dialog"
     width="800px"
-    :before-close="handleClose"
   >
     <div class="enterprise-dialog-box">
-      <div class="enterprise-dialog-box-title">发佣确认</div>
       <div class="enterprise-dialog-box-tip">
         <div>甲方&nbsp;&nbsp;武汉中新能科技有限公司</div>
         <div>乙方&nbsp;&nbsp;{{ formItem.channel_name }}</div>
       </div>
-      <table class="enterprise-dialog-box-content" cellspacing="0" border="0">
-        <tr>
-          <td>收佣渠道</td>
-          <td>{{ formItem.channel_name }}</td>
-        </tr>
-        <tr>
-          <td>收款银行</td>
-          <td>{{ formItem.bank }}</td>
-        </tr>
-        <tr>
-          <td>收款账户</td>
-          <td>{{ formItem.bank_account }}</td>
-        </tr>
-        <!--        <tr>-->
-        <!--          <td>渠道状态</td>-->
-        <!--          <td>收佣渠道</td>-->
-        <!--        </tr>-->
-        <tr v-if="formItem.confirm_time">
-          <td>确认时间时间</td>
-          <td>{{ formItem.confirm_time }}</td>
-        </tr>
-        <tr>
-          <td>渠道佣金税前</td>
-          <td>
+      <div class="settle-info">
+        <div class="settle-info-head">
+          <div class="settle-info-head-item amount">
+            <span class="settle-info-head-item-label font-bold text-size-16px"
+              >结算金额</span
+            >
             <money-text
-              :model-value="formItem.after_commission"
-              :mark-size="14"
-              :moneySize="14"
-              mark-color="#333333"
-              money-color="#333333"
+              :modelValue="formItem.settlement_amount"
+              markColor="#F35135"
+              moneyColor="#F35135"
+              moneySize="24"
+              markSize="24"
+              :bold="700"
             />
-          </td>
-        </tr>
-        <tr>
-          <td>渠道佣金税后</td>
-          <td>
+          </div>
+          <div class="settle-info-head-item">
+            <span class="settle-info-head-item-label">渠道佣金税前</span>
             <money-text
-              :model-value="formItem.after_commission"
-              :mark-size="14"
-              :moneySize="14"
-              mark-color="#333333"
-              money-color="#333333"
+              :modelValue="formItem.after_commission"
+              moneySize="18"
+              markSize="18"
+              :bold="600"
             />
-          </td>
-        </tr>
-        <tr>
-          <td>结算金额</td>
-          <td>
+          </div>
+          <div class="settle-info-head-item">
+            <span class="settle-info-head-item-label">渠道佣金税后</span>
             <money-text
-              :model-value="formItem.settlement_amount"
-              :mark-size="14"
-              :moneySize="14"
-              mark-color="#333333"
-              money-color="#333333"
+              :modelValue="formItem.after_commission"
+              moneySize="18"
+              markSize="18"
+              :bold="600"
             />
-          </td>
-        </tr>
-      </table>
+          </div>
+        </div>
+        <div class="settle-info-form">
+          <div class="settle-info-form-item">
+            <span>收佣渠道</span>
+            <span>{{ formItem.channel_name }}</span>
+          </div>
+          <div class="settle-info-form-item">
+            <span>收款银行</span>
+            <span>{{ formItem.bank }}</span>
+          </div>
+          <div class="settle-info-form-item">
+            <span>收款账户</span>
+            <span>{{ formItem.bank_account }}</span>
+          </div>
+        </div>
+      </div>
     </div>
     <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="HandleClose">取消</el-button>
-        <el-button type="primary" @click="handleConfirm"> 确认 </el-button>
-      </span>
+      <el-button v-if="hasFooter" @click="HandleClose">取消</el-button>
+      <el-button v-if="hasFooter" type="primary" @click="handleConfirm"
+        >确认</el-button
+      >
     </template>
   </el-dialog>
 </template>
@@ -85,6 +75,10 @@ const props = defineProps({
   dialogVisible: { type: Boolean, default: false },
   formItem: { type: Array, equired: true, default: () => {} },
   viewType: { type: Number, default: 0 },
+  hasFooter: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 let dialogVisible = computed(() => props.dialogVisible);
@@ -144,54 +138,74 @@ const handleClose = (done: () => void) => {
 </script>
 <style lang="scss" scoped>
 .enterprise-dialog {
-  .el-dialog__header {
-    display: none;
-  }
   .el-dialog__body {
     padding: 20px;
     padding-bottom: 0;
   }
-  &-box {
-    padding: 30px;
-    background-image: url("@/assets/watermark.png");
-    background-size: contain;
-    font-family: SourceHanSansSC-Medium, SourceHanSansSC, sans-serif;
-    &-title {
-      font-size: 28px;
-      font-weight: bold;
-      color: #333333;
-      text-align: center;
+}
+.enterprise-dialog-box {
+  &-tip {
+    display: flex;
+    > div {
+      width: 33%;
     }
-    &-tip {
+  }
+}
+.settle-info {
+  margin-top: 23px;
+  padding: 22px 24px 18px 24px;
+  background: rgba(243, 81, 53, 0.08);
+  &-head {
+    display: flex;
+    padding-bottom: 16px;
+    border-bottom: 1px solid rgba(243, 81, 53, 0.16);
+    &-item {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 62px;
+      font-family: SourceHanSansSC, SourceHanSansSC, sans-serif;
+      &-label {
+        font-size: 14px;
+        color: #656565;
+        &.text-size-16px {
+          font-size: 16px;
+          color: #333333;
+        }
+      }
+      .balance {
+        margin-left: 12px;
+        font-size: 14px;
+        font-family: SourceHanSansSC, SourceHanSansSC, sans-serif;
+        font-weight: 500;
+        color: #f36135;
+      }
+    }
+  }
+  .settle-info-form {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 10px;
+    &-item {
       display: flex;
       align-items: center;
-      justify-content: space-between;
-      padding: 10px 0;
-      font-size: 14px;
-      color: #3d4a52;
-    }
-    &-content {
-      width: 100%;
-      border-top: 1px solid #dcdfe6;
-      border-left: 1px solid #dcdfe6;
-      tr {
-        td {
-          border-right: 1px solid #dcdfe6;
-          border-bottom: 1px solid #dcdfe6;
-          &:first-child {
-            width: 26%;
-            text-align: center;
-            font-size: 14px;
-            font-weight: 500;
-            color: #656565;
-          }
-          &:last-child {
-            padding-left: 30px;
-            width: 74%;
-            font-size: 14px;
-            font-weight: 500;
-            color: #333333;
-          }
+      width: calc(100% / 3);
+      span {
+        &:last-child {
+          flex: 1 auto;
+          font-size: 14px;
+          font-family: SourceHanSansSC, SourceHanSansSC, sans-serif;
+          font-weight: 600;
+          color: #333333;
+        }
+        &:first-child {
+          flex: none;
+          width: 80px;
+          font-size: 14px;
+          font-family: SourceHanSansSC, SourceHanSansSC, sans-serif;
+          color: #656565;
         }
       }
     }
