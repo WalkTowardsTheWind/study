d622fd3633d967aa250aef644b30bdfb81387a5d
End <348956582@qq.com>
2024-02-05 17:35:12 +0800

    feat: :sparkles: 签署 证件类型

--- src/views/contract-management/components/1bus-c.vue
+++ src/views/contract-management/components/1bus-c.vue
@@ -61,11 +61,18 @@
       </template>
       <template #caozuo="{ row }">
         <el-button
-          v-if="row.status == 0"
+          v-if="row.status == 1"
           type="primary"
           link
           @click="goToSign(row.id)"
-          >发起签署</el-button
+          >签署</el-button
+        >
+        <el-button
+          v-if="row.status == 0"
+          type="primary"
+          link
+          @click="sendById(row.id)"
+          >发送文件</el-button
         >
         <el-button
           v-if="row.status == 0"
@@ -75,20 +82,26 @@
           >编辑</el-button
         >
         <el-button
-          v-if="row.status == 1"
+          v-if="row.status == 5"
           type="primary"
           link
           @click="setStatus(row.id, 0)"
           >撤回</el-button
         >
         <el-button
-          v-if="row.status != 1 && row.status != 2"
+          v-if="row.status == 1"
+          type="primary"
+          link
+          @click="backSend(row.id)"
+          >拒签</el-button
+        >
+        <el-button
+          v-if="row.status != 1 && row.status != 2 && row.status != 5"
           type="primary"
           link
           @click="delClick(row.id)"
           >删除</el-button
         >
-
         <el-button
           :disabled="!row.cert_url"
           v-if="row.status == 2 || row.status == 3"
@@ -146,25 +159,98 @@
     <!-- 发起签署 -->
     <zxn-dialog
       :visible="signVisible"
+      width="80vw"
+      top="5"
       :title="signTitle"
       @close-dialog="closeSign"
     >
       <template #default>
-        <el-form v-if="signStep == 1" label-width="auto">
-          <el-form-item required label="姓名">
-            <el-input v-model.trim="signForm.name" />
-          </el-form-item>
-          <el-form-item required label="身份证号">
-            <el-input maxlength="18" v-model="signForm.id_card" />
-          </el-form-item>
-        </el-form>
-        <template v-if="signStep == 2">
-          <p class="font-size-8">
-            已成功生成您的个人电子签名，是否签署合同并推送给对方？
-          </p>
-          <zxn-image :imgList="[signImage]"></zxn-image>
-        </template>
+        <div class="flex justify-between">
+          <div class="left flex-1">
+            <el-form label-width="100" label-position="right">
+              <el-row :gutter="50">
+                <el-col :span="12">
+                  <el-form-item label="合同类型">
+                    <el-input disabled value="企业合同" />
+                  </el-form-item>
+                  <el-form-item label="合同名称">
+                    <el-input disabled :value="signFormDetail.contract_name" />
+                  </el-form-item>
+                  <el-form-item label="合同模板">
+                    <el-input disabled :value="signFormDetail.template_name" />
+                  </el-form-item>
+                  <el-form-item label="合同编号">
+                    <el-input disabled :value="signFormDetail.contract_no" />
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item label="合同期限">
+                    <el-input
+                      disabled
+                      :value="
+                        signFormDetail.effective_start_time.substring(0, 10) +
+                        ' - ' +
+                        signFormDetail.effective_end_time.substring(0, 10)
+                      "
+                    />
+                  </el-form-item>
+                  <el-form-item label="甲方">
+                    <el-input disabled :value="signFormDetail.part_a" />
+                  </el-form-item>
+                  <el-form-item label="乙方">
+                    <el-input disabled :value="signFormDetail.part_b" />
+                  </el-form-item>
+                  <el-form-item label="备注要求">
+                    <el-input disabled :value="signFormDetail.remark" />
+                  </el-form-item>
+                </el-col>
+              </el-row>
+            </el-form>
+            <div class="bg-#f0f4ff h-55px flex items-center p-20px mb-20px">
+              合同签署信息
+            </div>
+            <el-form v-if="signStep == 1" label-width="100px">
+              <el-form-item required label="姓名">
+                <el-input v-model.trim="signForm.name" />
+              </el-form-item>
+              <el-form-item label="证据类型" required>
+                <el-select
+                  class="w-full"
+                  v-model="signForm.card_type"
+                  @change="
+                    () => {
+                      signForm.id_card = '';
+                    }
+                  "
+                >
+                  <el-option
+                    v-for="item of card_type"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item required label="证件号">
+                <el-input v-model.trim="signForm.id_card" />
+              </el-form-item>
+            </el-form>
+            <template v-if="signStep == 2">
+              <p class="font-size-8">
+                已成功生成您的个人电子签名，是否签署合同并推送给对方？
+              </p>
+              <zxn-image :imgList="[signImage]"></zxn-image>
+            </template>
+          </div>
+          <div class="right flex-1">
+            <iframe
+              class="iframe-url w-full h-70vh p-x-20px mb-20px"
+              :src="signFormDetail.contract_url"
+            ></iframe>
+          </div>
+        </div>
       </template>
+
       <template #bottom-btn>
         <div class="flex justify-center">
           <el-button
@@ -179,7 +265,7 @@
             v-if="signStep == 2"
             type="primary"
             @click="qshtfswj"
-            >签署合同并发送文件</el-button
+            >签署合同</el-button
           >
           <el-button type="primary" plain @click="closeSign">取消</el-button>
         </div>
@@ -193,6 +279,7 @@ import ContractAdd from "./contract-add.vue";
 import OnlineSign from "./online-sign.vue";
 import ContractDetail from "./contract-detail.vue";
 import ContractEdit from "./contract-edit.vue";
+import { card_type } from "./options";
 
 import {
   getContractList,
@@ -200,6 +287,9 @@ import {
   delContract,
   createContractSeal,
   goContractOnline,
+  sendContract,
+  rejectContract,
+  contractDetail,
 } from "@/api/contract-m/index";
 
 import { contract_status, color } from "./options";
@@ -249,8 +339,8 @@ const handleSearch = () => {
 const detailId = ref(0);
 const tableData = reactive([] as any);
 const columnList = [
-  { label: "合同编号", prop: "contract_no" },
-  { label: "合同名称", prop: "contract_name" },
+  { label: "合同编号", prop: "contract_no", minWidth: 120 },
+  { label: "合同名称", prop: "contract_name", minWidth: 100 },
   { label: "合同类型", slot: "type" },
   {
     label: "状态",
@@ -264,10 +354,11 @@ const columnList = [
   { label: "乙方", prop: "part_b_name" },
   { label: "签约时间", prop: "sign_time", minWidth: 150 },
   { label: "到期时间", prop: "effective_end_time", minWidth: 150 },
+  { label: "拒签原因", prop: "reason", minWidth: 150 },
   {
     label: "操作",
     slot: "caozuo",
-    minWidth: 250,
+    minWidth: 300,
     align: "right",
     fixed: "right",
   },
@@ -382,12 +473,25 @@ const isLoading = ref(false);
 const signForm = reactive({
   name: "",
   id_card: "",
+  card_type: 19,
+});
+const signFormDetail = reactive({
+  contract_name: "",
+  template_name: "",
+  contract_no: "",
+  effective_start_time: "",
+  effective_end_time: "",
+  part_a: "",
+  part_b: "",
+  remark: "",
+  contract_url: "",
 });
 const closeSign = () => {
   signVisible.value = false;
   signStep.value = 1;
   signForm.name = "";
   signForm.id_card = "";
+  signForm.card_type = 19;
   cfi.psn_id = "";
   cfi.seal = "";
   isLoading.value = false;
@@ -395,9 +499,21 @@ const closeSign = () => {
 const goToSign = (id) => {
   // 1 生成印章
   // createContractSeal();
-  signVisible.value = true;
   signTitle.value = "填写负责人信息";
-  cfi.id = id;
+  contractDetail(id).then((res) => {
+    signFormDetail.contract_name = res.data.contract_name;
+    signFormDetail.template_name = res.data.template_name;
+    signFormDetail.contract_no = res.data.contract_no;
+    signFormDetail.contract_url = res.data.contract_url;
+    signFormDetail.effective_start_time = res.data.effective_start_time;
+    signFormDetail.effective_end_time = res.data.effective_end_time;
+    signFormDetail.part_a = res.data.part_a_name;
+    signFormDetail.part_b = res.data.part_b_name;
+    signFormDetail.remark = res.data.remark;
+
+    signVisible.value = true;
+    cfi.id = id;
+  });
 };
 
 const scgrdzqm = () => {
@@ -472,5 +588,40 @@ const handleExport = async () => {
   await downloadByOnlineUrl("/adminapi/contract/get_contract_download", {
     ids: ids.value,
   });
+};
+
+const sendById = (id) => {
+  ElMessageBox.confirm("是否发送文件?", {
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+  })
+    .then(() => {
+      sendContract(id).then(() => {
+        ElMessage.success("操作成功！");
+        handleSearch();
+      });
+    })
+    .catch(() => {
+      ElMessage.info("取消操作");
+    });
+};
+
+const backSend = (id) => {
+  ElMessageBox.prompt("请输入拒签原因", {
+    confirmButtonText: "确认",
+    cancelButtonText: "取消",
+  })
+    .then(({ value }) => {
+      rejectContract({ id, reason: value }).then(() => {
+        ElMessage.success("操作成功");
+        handleSearch();
+      });
+    })
+    .catch(() => {
+      ElMessage({
+        type: "info",
+        message: "取消操作",
+      });
+    });
 };
 </script>
