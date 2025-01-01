56654799f6a83d83813e1cb51c3297faa8cbbc27
柳源 <ly1271512676@163.com>
2025-01-01 21:48:47 +0800

    feat: :sparkles: 输入框优化

--- src/components/BaseInput/BaseInput.vue
+++ src/components/BaseInput/BaseInput.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <el-dropdown ref="dropdown" trigger="click" placement="top">
-      <div v-if="!value" class="header">
+      <div v-if="!activeValue" class="header">
         <div>{{ title }}</div>
         <!-- <img style="width: 20px; height: 20px;" src="../../../assets/avatar.png" /> -->
       </div>
       <div v-else class="selected">
         <div class="lable">
-          {{ value }}
+          {{ activeValue }}
           <i-ep-CircleClose
             style="color: #000; margin-left: 10px"
             @click="clearValue"
@@ -19,7 +19,7 @@
         <!-- @clear="clearValue"  @blur="closeDropdown"-->
         <el-input
           ref="input"
-          v-model="activeValue"
+          v-model.trim="activeValue"
           @change="updateActiveValue"
           @blur="closeDropdown"
           style="width: 120px"
@@ -36,11 +36,11 @@ import { defineProps, defineEmits } from "vue";
 
 // 接收父组件传递的 `value` 和 `title`
 const props = defineProps({
-  title: {
+  modelValue: {
     type: String,
     required: true,
   },
-  value: {
+  title: {
     type: String,
     required: true,
   },
@@ -52,16 +52,17 @@ const closeDropdown = () => {
   dropdown.value?.handleClose();
 };
 // 发出 `update:value` 事件，通知父组件更新 `value`
-const emit = defineEmits(["update:value", "data-change"]);
-const activeValue = ref(props.value);
+const emit = defineEmits(["update:modelValue", "data-change"]);
+// const activeValue = ref(props.value);
+const activeValue = useVModel(props, "modelValue", emit);
 // 清空值
 const clearValue = () => {
+  // activeValue.value=''
   input.value?.clear();
 };
 
 // 通过 input 事件更新父组件的值
 const updateActiveValue = (value) => {
-  emit("update:value", value); // 保证双向绑定
   emit("data-change", 2);
   if (value) {
     input.value?.blur();
