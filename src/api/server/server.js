f308c0baced116443888d352b4872f5ac538093c
柳源 <ly1271512676@163.com>
2025-01-02 23:01:20 +0800

    feat: :sparkles: get服务

---
+++ src/api/server/server.js
@@ -0,0 +1,27 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Allow requests only from this origin
+// }));
+
+// // 定义一个 GET 请求处理
+// app.get('/api/data', (req, res) => {
+//   const data = {
+//     msg:'ly',
+//     status:200,
+//     name: 'John Doe',
+//     age: 30,
+//     occupation: 'Software Developer'
+//   };
+
+//   // 返回对象
+//   res.json(data);
+// });
+
+// 设置端口监听
+// const port = 3003;
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
